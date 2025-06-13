11886d26854f85a3336b373737fc75ec528de274
DesconocidoCL <agustinvillablanca.t@gmail.com>
2025-06-13 01:39:28 -0400

    Update app.py
    probando arreglo

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Forzando un redespliegue
 from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory
 from werkzeug.security import generate_password_hash
 from werkzeug.utils import secure_filename
